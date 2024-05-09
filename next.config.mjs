040da4c6573bc14edfd9dbf34564efdf2dac1b24
Casper Anisimow <81159246+anisthepooh@users.noreply.github.com>
2024-05-09 18:45:03 +0200

    add pwa

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,15 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  reactStrictMode: true,
+};
 
-export default nextConfig;
+import withPWAInit from "@ducanh2912/next-pwa";
+
+const withPWA = withPWAInit({
+  dest: "public",
+});
+
+export default withPWA({
+  nextConfig
+});
+

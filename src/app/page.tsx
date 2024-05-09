dc1ba094019b1783cf5b09e018fbd9195b1669d0
Casper Anisimow <81159246+anisthepooh@users.noreply.github.com>
2024-05-09 11:39:23 +0200

    Prepering app like feeling

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,29 +4,47 @@ import Counter from "./components/Counter"
 import { useState } from "react";
 import Alert from "./components/Alert"
 import { CircularProgress } from "@nextui-org/progress";
+import { AuroraBackground } from "../components/ui/aurora-background"
+import { motion } from "framer-motion";
+import preventPinchZoom from "@/lib/preventPinchZoom";
 
 export default function Home() {
   const [isRunning, setIsRunning] = useState(false)
   const [rounds, setRounds] = useState(0)
-
+  
+  preventPinchZoom()
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24 bg-yellow-200">
-      <CircularProgress
-          classNames={{
-            svg: "w-36 h-36 drop-shadow-md",
-            indicator: "stroke-white",
-            track: "stroke-white/50",
-            value: "text-3xl font-semibold text-white",
-          }}
-          value={(rounds)}
-          maxValue={60}
-          strokeWidth={4}
-          showValueLabel={true}
-        />    
-      <p>Roundes: {rounds}</p>
-      <Counter isRunning={isRunning} setIsRunning={setIsRunning} setRounds={setRounds} rounds={rounds} />
-      <Button onClick={() => setIsRunning(!isRunning)}>{isRunning ? "Stop" : "Start"}</Button>
-      <Alert rounds={rounds} isRunning={isRunning} setIsRunning={setIsRunning} />
-    </main>
+    <AuroraBackground>
+      <motion.div
+        initial={{ opacity: 0.0, y: 40 }}
+        whileInView={{ opacity: 1, y: 0 }}
+        transition={{
+          delay: 0.3,
+          duration: 0.8,
+          ease: "easeInOut",
+        }}
+        className="h-full relative flex flex-col gap-4 items-center justify-center px-4"
+      >
+        <main className="flex flex-col items-center justify-between">
+          <CircularProgress
+              classNames={{
+                svg: "w-36 h-36 drop-shadow-md",
+                indicator: "stroke-white",
+                track: "stroke-white/50",
+                value: "text-3xl font-semibold text-white",
+              }}
+              value={(rounds)}
+              maxValue={60}
+              strokeWidth={4}
+              showValueLabel={true}
+            />    
+          <p>Roundes: {rounds}</p>
+          <Counter isRunning={isRunning} setIsRunning={setIsRunning} setRounds={setRounds} rounds={rounds} />
+          <Button onClick={() => setIsRunning(!isRunning)}>{isRunning ? "Stop" : "Start"}</Button>
+          <Alert rounds={rounds} isRunning={isRunning} setIsRunning={setIsRunning} />
+        </main>
+      </motion.div>
+    </AuroraBackground>
+
   );
 }
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,8 +1,14 @@
 import type { Config } from "tailwindcss"
 const {nextui} = require("@nextui-org/react");
+const {
+  default: flattenColorPalette,
+} = require("tailwindcss/lib/util/flattenColorPalette");
 
 
 const config = {
+  future: {
+    hoverOnlyWhenSupported: true,
+  },
   darkMode: ["class"],
   content: [
     './pages/**/*.{ts,tsx,js}',
@@ -62,6 +68,14 @@ const config = {
         sm: "calc(var(--radius) - 4px)",
       },
       keyframes: {
+        aurora: {
+          from: {
+            backgroundPosition: "50% 50%, 50% 50%",
+          },
+          to: {
+            backgroundPosition: "350% 50%, 350% 50%",
+          },
+        },
         "accordion-down": {
           from: { height: "0" },
           to: { height: "var(--radix-accordion-content-height)" },
@@ -72,12 +86,24 @@ const config = {
         },
       },
       animation: {
+        aurora: "aurora 60s linear infinite",
         "accordion-down": "accordion-down 0.2s ease-out",
         "accordion-up": "accordion-up 0.2s ease-out",
       },
     },
   },
-  plugins: [require("tailwindcss-animate"), nextui()],
+  plugins: [require("tailwindcss-animate"), nextui(), addVariablesForColors],
 } satisfies Config
+
+function addVariablesForColors({ addBase, theme }: any) {
+  let allColors = flattenColorPalette(theme("colors"));
+  let newVars = Object.fromEntries(
+    Object.entries(allColors).map(([key, val]) => [`--${key}`, val])
+  );
+ 
+  addBase({
+    ":root": newVars,
+  });
+}
 
 export default config

213cea862224d2a02a36d0ebb489a180337003bc
Casper Anisimow <81159246+anisthepooh@users.noreply.github.com>
2024-05-10 15:04:22 +0200

    remove bg color from body

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -41,7 +41,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en" className="overscroll-y-none touch-none	">
-      <body className="bg-green-500">
+      <body className={inter.className}>
         <NextUIProvider>
           {children}
         </NextUIProvider>
